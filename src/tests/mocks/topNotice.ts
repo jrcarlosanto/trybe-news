8771409373241c27463ea83daf95a4d843254ed7
Carlos Antonio Sales de Oliveira Junior <carlosamtoni@gmail.com>
2023-11-17 22:27:56 -0300

    test: Teste da page Headlines

---
+++ src/tests/mocks/topNotice.ts
@@ -0,0 +1,15 @@
+export const topNotice = {
+  id: 38394,
+  tipo: 'Notícia',
+  titulo: '"A luta do IBGE chega a quem mais precisa, à Dona Maria e Seu José”',
+  introducao: 'A ministra Simone Tebet enviou vídeo para os participantes do encontro.',
+  produto_id: 0,
+  produtos: '',
+  editorias: 'ibge',
+  imagens: '{"image_intro":"images\\/agenciadenoticias\\/ibge\\/2023_11'
+    + '\\/Tebet_Dialogos_THUMB.jpg"}',
+  produtos_relacionados: '',
+  destaque: true,
+  data_publicacao: '17/11/2023 14:45:00',
+  link: 'http://agenciadenoticias.ibge.gov.br/agencia-noticias/2012-agencia-de-noticias/noticias/38394-a-luta-do-ibge-chega-a-quem-mais-precisa-a-dona-maria-e-seu-jose-diz-ministra-simone-tebet-em-mensagem-ao-1-encontro-dialogos-ibge-90-anos.html',
+};
