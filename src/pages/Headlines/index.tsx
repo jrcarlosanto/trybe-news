5529f0526a354e22aca83f00faa2c4cca9dffea5
Carlos Antonio Sales de Oliveira Junior <carlosamtoni@gmail.com>
2023-11-20 23:29:29 -0300

    feat: Atualização da page Headlines

--- src/pages/Headlines/index.tsx
+++ src/pages/Headlines/index.tsx
@@ -1,10 +1,13 @@
+import ListNotice from '../../components/ListMotices';
+import Toolbar from '../../components/Toolbar';
 import TopNotice from '../../components/TopNotice';
 
 function Headlines() {
   return (
     <>
       <TopNotice />
-      <section>Ok</section>
+      <Toolbar />
+      <ListNotice />
     </>
   );
 }
